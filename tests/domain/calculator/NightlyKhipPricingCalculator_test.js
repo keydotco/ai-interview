41b5591830d5e06ee04d0d7bebb270894087f314
Jacob Daily <60455040+jdaily-dev@users.noreply.github.com>
2025-05-07 16:32:00 -0500

    remove specific test assertions for nightly price challanege

--- tests/domain/calculator/NightlyKhipPricingCalculator_test.js
+++ tests/domain/calculator/NightlyKhipPricingCalculator_test.js
@@ -12,30 +12,6 @@ describe('NightlyKhipPricingCalculator', () => {
 
       expect(result).to.be.an('array');
       expect(result.length).to.equal(17);
-
-      expect(result[1]).to.deep.equal({
-        start: '2025-05-10',
-        end: '2025-05-11',
-        nightlyRate: 460,
-      });
-      
-      expect(result[2]).to.deep.equal({
-        start: '2025-05-12',
-        end: '2025-05-16',
-        nightlyRate: 450,
-      });
-      
-      expect(result[14]).to.deep.equal({
-        start: '2025-07-27',
-        end: '2025-07-31',
-        nightlyRate: 365,
-      });
-      
-      expect(result[16]).to.deep.equal({
-        start: '2025-08-26',
-        end: '2027-05-07',
-        nightlyRate: 300,
-      });
     });
   });
 });
